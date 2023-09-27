b994bb04ef7102dd46d1ed73e9329d7addba56a0
christo2001 <christolawrencee@email.com>
2023-09-27 20:17:58 +0530

    forgetpassword

--- node/forgetpassword/models/forget.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require("mongoose");
-
-// const checkemailSchema = new mongoose.Schema({
-//     email:String
-// })
-
-// const Checkemail = mongoose.model("Checkemail",checkemailSchema)
-
-// module.exports = Checkemail;
