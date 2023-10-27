b1ca1bda84f65b1a35d18e61d9c21b9a2b62a72b
christo2001 <christolawrencee@email.com>
2023-10-27 20:30:41 +0530

    sum1

--- node/summa/backend/server.js
+++ node/summa/backend/server.js
@@ -17,13 +17,50 @@ const U = require("./models/r");
 
 const PORT = process.env.port
 const MONGO_URL = process.env.url
-const SECRET_KEY = process.env.SECRET_KEY;
+
 
 //Connect to MongoDB
 mongoose
   .connect(MONGO_URL, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Could not connect to MongoDB", err));
+
+
+  app.post('/reg', async (req, res) => {
+    const { firstname } = req.body;
+  
+    const userss = new U({ firstname }); // Change "User" to "U"
+  
+    try {
+      await userss.save();
+  
+      res.json({ message: "User registered successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "An error occurred while registering the user!" });
+    }
+  });
+
+  app.get('/user/:userId', async (req, res) => {
+    const userId = req.params.userId;
+  
+    try {
+      const user = await U.findById(userId); // Assuming you have a model called "U" for users
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "An error occurred while fetching user data" });
+    }
+  });
+  
+
+  
+  
 
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT);
