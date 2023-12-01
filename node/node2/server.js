5fbbf493cfba78d78eeff93ccae0621274a6ea12
christo2001 <christolawrencee@gmail.com>
2023-12-01 19:35:28 +0530

    first commit

--- node/node2/server.js
+++ node/node2/server.js
@@ -4,131 +4,109 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// In-memory data storage
+const rooms = [];
+const bookings = [];
+const customers = [];
+
+// Middleware for parsing JSON requests
 app.use(bodyParser.json());
 
-// room
-const rooms = [];
-
-app.post('/room', (req, res) => {
-  const { seats, amenities, pricePerHour } = req.body;
-
-  if (!seats || !pricePerHour) {
-    return res.status(400).json({ error: 'Seats and pricePerHour are required' });
-  }
+// 1. Create a room
+app.post('/rooms', (req, res) => {
+  const { roomName, seatsAvailable, amenities, pricePerHour } = req.body;
 
   const room = {
-    seats,
+    id: rooms.length + 1,
+    roomName,
+    seatsAvailable,
     amenities,
     pricePerHour,
   };
 
   rooms.push(room);
-
-  res.status(201).json(room);
+  res.json(room);
 });
 
-// Define a route to get all rooms
-app.get('/room', (req, res) => {
-  res.json(rooms);
-});
-
-
-//----------------------------------------------------------------------------------------  
-
-const roomdetails = [
-  {roomname:"zenova", bookingstatus:"success", customername:"christo", date:"14-09-2023",start_time:"13.00",end_time:"17.00"},
-  {roomname:"gateway", bookingstatus:"success", customername:"witney", date:"15-09-2023",start_time:"18.00",end_time:"22.00"},
-  {roomname:"ginsberg", bookingstatus:"failed", customername:"sasi", date:"25-09-2023",start_time:"08.00",end_time:"12.00"},
-]
-
-app.get('/getroomdetails',(req,res)=>{
-  res.json(roomdetails)
-})
-
-
-//---------------------------------------------------------------------------------------------------- 
-
-
-const roombooking = [];
-
+// 2. Book a room
 app.post('/bookings', (req, res) => {
-  const { customer_name, date, start_time, end_time, room_id } = req.body;
+  const { customerName, date, startTime, endTime, roomId } = req.body;
 
-  if (!customer_name || !date || !start_time || !end_time || !room_id) {
-    return res.status(400).json({ error: 'Customer name, date, start time, end time, and room id are required' });
+  const room = rooms.find((r) => r.id === roomId);
+  if (!room) {
+    return res.status(400).json({ error: 'Room not found' });
   }
 
-  // Check if there's already a booking for the same room with overlapping time
-  const isOverlap = roombooking.some((booking) => {
-    if (booking.room_id === room_id && booking.date === date) {
-      if (
-        (start_time >= booking.start_time && start_time < booking.end_time) ||
-        (end_time > booking.start_time && end_time <= booking.end_time)
-      ) {
-        return true;
-      }
-    }
-    return false;
-  });
-
-  if (isOverlap) {
-    return res.status(400).json({ error: 'Room is already booked for the specified date and time' });
-  }
-
-  const roombookings = {
-    customer_name,
+  const booking = {
+    id: bookings.length + 1,
+    roomName: room.roomName,
+    customerName,
     date,
-    start_time,
-    end_time,
-    room_id,
+    startTime,
+    endTime,
+    roomId,
   };
 
-  roombooking.push(roombookings);
-
-  res.status(201).json(roombookings);
+  bookings.push(booking);
+  res.json(booking);
 });
 
-// Define a route to get all room bookings
-app.get('/bookings', (req, res) => {
-  res.json(roombooking);
+// 3. List all rooms with booked data
+app.get('/rooms/booked-data', (req, res) => {
+  const result = rooms.map((room) => {
+    const booking = bookings.find((b) => b.roomId === room.id);
+    return {
+      roomName: room.roomName,
+      bookedStatus: booking ? 'Booked' : 'Available',
+      customerName: booking ? booking.customerName : null,
+      date: booking ? booking.date : null,
+      startTime: booking ? booking.startTime : null,
+      endTime: booking ? booking.endTime : null,
+    };
+  });
+
+  res.json(result);
 });
 
-//------------------------------------------------------------------------------------------------------------
+// 4. List all customers with booked data
+app.get('/customers/booked-data', (req, res) => {
+  const result = bookings.map((booking) => {
+    const room = rooms.find((r) => r.id === booking.roomId);
+    return {
+      roomName: room.roomName,
+      customerName: booking.customerName,
+      date: booking.date,
+      startTime: booking.startTime,
+      endTime: booking.endTime,
+    };
+  });
 
-
-const customerdetails = [
-  {roomname:"zenova", customername:"christo", date:"14-09-2023",start_time:"13.00",end_time:"17.00"},
-  {roomname:"gateway", customername:"witney", date:"15-09-2023",start_time:"18.00",end_time:"22.00"},
-  {roomname:"ginsberg", customername:"sasi", date:"25-09-2023",start_time:"08.00",end_time:"12.00"},
-]
-
-app.get('/getcustomerdetails',(req,res)=>{
-  res.json(customerdetails)
-})
-
-//-------------------------------------------------------------------------------------------------------- 
-
-
-const bookingdetails = [
-  {roomname:"valhalla", customername:"christo", date:"30-09-2023",start_time:"16.00",end_time:"17.00",bookingdate:"30-09-2023",
-  bookingid:1,bookingstatus:"success"
-  },
-  {roomname:"zenova", customername:"christo", date:"14-09-2023",start_time:"13.00",end_time:"17.00",bookingdate:"14-09-2023",
-  bookingid:1,bookingstatus:"success"
-  },
-  {roomname:"zodiac", customername:"witney", date:"27-09-2023",start_time:"17.00",end_time:"23.00",bookingdate:"27-09-2023",
-  bookingid:1,bookingstatus:"success"
-  },
-]
-
-app.get('/getbookingdetails/:name', (req, res) => {
-  const name = req.params.name;
-  const customerBookings = bookingdetails.filter((booking) => booking.customername === name);
-  res.json(customerBookings);
+  res.json(result);
 });
 
-//-----------------------
+// 5. List customer booking details
+app.get('/customers/:customerId/bookings', (req, res) => {
+  const customerId = parseInt(req.params.customerId);
+
+  const result = bookings
+    .filter((booking) => booking.customerId === customerId)
+    .map((booking) => {
+      const room = rooms.find((r) => r.id === booking.roomId);
+      return {
+        roomName: room.roomName,
+        customerName: booking.customerName,
+        date: booking.date,
+        startTime: booking.startTime,
+        endTime: booking.endTime,
+        bookingId: booking.id,
+        bookingDate: booking.bookingDate,
+        bookingStatus: booking.bookingStatus,
+      };
+    });
+
+  res.json(result);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
