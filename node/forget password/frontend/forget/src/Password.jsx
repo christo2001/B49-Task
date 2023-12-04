bb752ce80a3885133f8372c0ee8932061dce2ae3
christo2001 <christolawrencee@gmail.com>
2023-12-04 22:23:04 +0530

    forget

--- node/forget password/frontend/forget/src/Password.jsx
+++ node/forget password/frontend/forget/src/Password.jsx
@@ -2,51 +2,53 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import img2 from '../images/image3.png';
 import passwordcss from './password.module.css';
-import { useParams,useNavigate } from 'react-router-dom';
-
+import { useParams, useNavigate } from 'react-router-dom';
 
 function Password() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
-  const [newpassword, setNewPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState('');
   const { token } = useParams();
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      setEmail(e.target.value);
-    } else if (e.target.name === 'password') {
-      setNewPassword(e.target.value);
-    }
-  };
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  if (name === 'email') {
+    setEmail(value);
+  } else if (name === 'newPassword') {
+    setNewPassword(value);
+  }
+};
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(`https://forget-password-2zs6.onrender.com/api/user/changepassword/${token}`, {
-        email: email,  // Ensure this property is named 'email'
-        newPassword: newpassword,  // Ensure this property is named 'newPassword'
-      });
-      
+      const response = await axios.post(
+        `https://forget-password-2zs6.onrender.com/api/user/changepassword/${token}`,
+        {
+          email: email, // Ensure this property is named 'email'
+          newPassword: newPassword, // Ensure this property is named 'newPassword'
+        }
+      );
 
-      console.log("Success:", response.data);
-      setSuccessMessage("Password changed successfully!");
+      console.log('Success:', response.data);
+      setSuccessMessage('Password changed successfully!');
       setError(null);
-       // Navigate to login after a 2-second delay
-       const delayInSeconds = 2;
-       const intervalId = setInterval(() => {
-         clearInterval(intervalId);
-         navigate('/login');
-       }, delayInSeconds * 1000);
+      // Navigate to login after a 2-second delay
+      const delayInSeconds = 2;
+      const intervalId = setInterval(() => {
+        clearInterval(intervalId);
+        navigate('/login');
+      }, delayInSeconds * 1000);
     } catch (error) {
-      console.error("Error:", error);
-      setError(error.response?.data?.message || "An error occurred");
+      console.error('Error:', error);
+      setError(error.response?.data?.message || 'An error occurred');
       setSuccessMessage('');
     }
   };
-
   return (
     <div className={passwordcss.body}>
       <section className={passwordcss.side}>
