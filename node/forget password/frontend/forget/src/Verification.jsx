596fcc971d6a7233e4044a1dcc1576f256391a10
christo2001 <christolawrencee@gmail.com>
2023-12-05 19:55:35 +0530

    forget

--- node/forget password/frontend/forget/src/Verification.jsx
+++ node/forget password/frontend/forget/src/Verification.jsx
@@ -2,16 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import verifycss from './verify.module.css';
 
-// ... (previous imports)
-
 const Verification = ({ token }) => {
   const [verificationStatus, setVerificationStatus] = useState(null);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const verifyUser = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/user/verify/:token`);
+        // Update the URL to include the token dynamically
+        const response = await fetch(`http://localhost:3000/api/user/verify/${token}`);
         const data = await response.json();
 
         if (response.ok) {
@@ -20,8 +20,11 @@ const Verification = ({ token }) => {
           setVerificationStatus(data.error || 'Verification failed.');
         }
       } catch (error) {
+        // Display the actual error message from the server
         console.error('Error during verification:', error);
-        setVerificationStatus('An error occurred during verification.');
+        setVerificationStatus(`An error occurred during verification: ${error.message}`);
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or failure
       }
     };
 
@@ -30,16 +33,21 @@ const Verification = ({ token }) => {
 
   return (
     <div className={verifycss.verifybox}>
-      {verificationStatus ? (
-        <>
-          <p className={verifycss.verifymessage}>{verificationStatus}</p>
-          {verificationStatus === 'User successfully verified!' && <Link to="/login" className={verifycss.verifybutton}>Go to Login</Link>}
-        </>
+      {loading ? (
+        <p>Verifying user....</p>
       ) : (
         <>
-        <p>verifying user....</p>
           <p className={verifycss.verifymessage}>{verificationStatus}</p>
-          {verificationStatus === 'Verification failed' && <p>verification failed</p>}
+          {verificationStatus && (
+            <>
+              {verificationStatus.includes('User successfully verified') && (
+                <Link to="/login" className={verifycss.verifybutton}>
+                  Go to Login
+                </Link>
+              )}
+              {verificationStatus.includes('Verification failed') && <p>Verification failed</p>}
+            </>
+          )}
         </>
       )}
     </div>
