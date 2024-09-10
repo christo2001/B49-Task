ecf893d029f21d81b86aed069470a581b7ce0c55
christo2001 <christolawrencee@gmail.com>
2024-09-10 16:04:06 +0530

    maps

--- node/practice/gps/src/MapComponent.jsx
+++ node/practice/gps/src/MapComponent.jsx
@@ -4,9 +4,11 @@ import { GoogleMap, Marker, Polyline, useLoadScript } from '@react-google-maps/a
 const MapComponent = () => {
   const [vehiclePosition, setVehiclePosition] = useState(null);
   const [path, setPath] = useState([]);
+  const [destination, setDestination] = useState(null);
+  const speed = 0.00001; // Adjust the speed of movement
 
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyAlc2pH3emh40cj44TleEDBEwrG1GZnqP4" // Remove the extra space
+    googleMapsApiKey: "AIzaSyAlc2pH3emh40cj44TleEDBEwrG1GZnqP4"
   });
 
   useEffect(() => {
@@ -14,17 +16,44 @@ const MapComponent = () => {
       const response = await fetch('http://localhost:4000/vehicle-location');
       const data = await response.json();
       console.log('Vehicle Data:', data); // Log the data for debugging
-      setVehiclePosition({ lat: data.latitude, lng: data.longitude });
-      setPath((prevPath) => [...prevPath, { lat: data.latitude, lng: data.longitude }]);
+      
+      // Set destination and initial vehicle position
+      const newDestination = { lat: data.latitude, lng: data.longitude };
+      if (!vehiclePosition) {
+        setVehiclePosition(newDestination);
+        setDestination(newDestination);
+      } else {
+        setDestination(newDestination);
+      }
+
+      // Update path
+      setPath((prevPath) => [...prevPath, newDestination]);
     }, 5000); // Update every 5 seconds
 
     return () => clearInterval(interval);
-  }, []);
+  }, [vehiclePosition]);
+
+  useEffect(() => {
+    if (destination) {
+      const moveInterval = setInterval(() => {
+        setVehiclePosition((prevPosition) => {
+          if (prevPosition.lat === destination.lat && prevPosition.lng === destination.lng) {
+            clearInterval(moveInterval);
+            return prevPosition; // Stop moving if destination is reached
+          }
+
+          const newLat = prevPosition.lat + (destination.lat - prevPosition.lat) * speed;
+          const newLng = prevPosition.lng + (destination.lng - prevPosition.lng) * speed;
+
+          return { lat: newLat, lng: newLng }; // Move towards destination
+        });
+      }, 100); // Move every 100 ms
+
+      return () => clearInterval(moveInterval);
+    }
+  }, [destination]);
 
   if (!isLoaded) return <div>Loading...</div>;
-
-  // URL of the car icon image
-  const carIcon = 'https://example.com/path/to/car-icon.png'; // Replace with your car image URL
 
   return (
     <GoogleMap
@@ -34,10 +63,7 @@ const MapComponent = () => {
     >
       {vehiclePosition && (
         <>
-          <Marker 
-            position={vehiclePosition} 
-            icon={carIcon} // Set the car image as the marker icon
-          />
+          <Marker position={vehiclePosition} />
           <Polyline path={path} options={{ strokeColor: "red" }} />
         </>
       )}
